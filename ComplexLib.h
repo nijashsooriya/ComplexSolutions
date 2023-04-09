17e4116ce9951b980620115641d0447d75a7aa62
Nijash Sooriya <44658563+nijashsooriya@users.noreply.github.com>
2023-04-08 23:18:17 -0400

    Calculate specific Pythagorean Triples, uses Number Theory and Complex Library in this repo to achieve O(n) runtime

---
+++ ComplexLib.h
@@ -0,0 +1,8 @@
+//
+// Created by Nijash Sooriya on 2023-04-08.
+//
+
+#ifndef UNTITLED3_COMPLEXLIB_H
+#define UNTITLED3_COMPLEXLIB_H
+
+#endif //UNTITLED3_COMPLEXLIB_H
---
+++ PythagoreanTriple.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Nijash Sooriya on 2023-04-08.
+//
+#include <iostream>
+#include <vector>
+#include "ComplexLib.h"
+
+using namespace std;
+
+vector<vector<int>> triples(){
+    vector<vector<int>> result;
+    for(int i = 2; i < 100; i++){
+        Complex n1(1, i);
+        vector<int> temp;
+        Complex resultant = multiply(n1, n1);
+        temp.push_back(abs(getReal(resultant)));
+        temp.push_back(abs(getImag(resultant)));
+        temp.push_back(getMag(resultant));
+        result.push_back(temp);
+    }
+
+
+
+    return result;
+}
+
+int main(){
+    vector<vector<int>> return_vect = triples();
+    for(int i = 0; i < return_vect.size(); i++){
+        std::cout << return_vect[i][0] << ", " << return_vect[i][1] << ", " << return_vect[i][2] << '\n';
+    }
+
+}
